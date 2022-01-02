c31d8b903fb66c099b578feb04c5a64f370b4f95
pavelchernov <p.chernov@city-mobil.ru>
2022-01-02 14:24:41 +0300

    добавил переходы между фрагментами вынес размер текста в ресурсы

--- app/src/main/java/com/example/notesapp/ui/MainActivity.java
+++ app/src/main/java/com/example/notesapp/ui/MainActivity.java
@@ -1,15 +1,47 @@
 package com.example.notesapp.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import com.example.notesapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity implements EditNoteFragment.Controller {
 
+    private BottomNavigationView bottomNavigationView;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+
+        bottomNavigationView = findViewById(R.id.main_bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener((NavigationBarView.OnItemSelectedListener) item -> {
+
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.bottom_menu_settings:
+                    fragment = new SettingsFragment();
+                    break;
+                case R.id.bottom_menu_calendar:
+                    fragment = new CalendarFragment();
+                    break;
+                default:
+                    fragment = new NotesListFragment();
+                    break;
+            }
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        });
 
         NotesListFragment notesListFragment = new NotesListFragment();
         getSupportFragmentManager()
