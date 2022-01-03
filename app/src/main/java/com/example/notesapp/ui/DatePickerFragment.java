b844e615351b5d2a3b500ad14b9ed206a7c0b896
pavelchernov <p.chernov@city-mobil.ru>
2022-01-03 12:56:04 +0300

    добавил переменную в которую сохраняется выбранная дата

--- app/src/main/java/com/example/notesapp/ui/DatePickerFragment.java
+++ app/src/main/java/com/example/notesapp/ui/DatePickerFragment.java
@@ -1,20 +1,30 @@
 package com.example.notesapp.ui;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import com.example.notesapp.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class DatePickerFragment extends Fragment {
 
     private DatePicker datePicker;
+    private Button confirmDate;
+    private String date;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,10 +32,24 @@ public class DatePickerFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_date_picker, container, false);
     }
 
+    @SuppressLint("SimpleDateFormat")
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onViewCreated(@NonNull @org.jetbrains.annotations.NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         datePicker = view.findViewById(R.id.date_picker);
+        confirmDate = view.findViewById(R.id.confirm_date);
 
+        date = new SimpleDateFormat("d/M/yyyy").format(Calendar.getInstance().getTime());
+
+        datePicker.setOnDateChangedListener((datePicker, year, month, dayOfMonth) ->
+                date = String.format(Locale.ENGLISH, "%d/%d/%d", dayOfMonth, month + 1, year));
+
+        confirmDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
